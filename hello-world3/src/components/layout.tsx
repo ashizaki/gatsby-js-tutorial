1b51a348acde15156185d2a7045bca3d586269ef
Keisuke Ashizaki <ashizaki_keisuke@mbpsoft.co.jp>
2020-05-10 21:36:16 +0900

    Modify

--- hello-world3/src/components/layout.tsx
+++ hello-world3/src/components/layout.tsx
@@ -1,8 +1,28 @@
 import * as React from 'react'
+import { Link } from "gatsby"
+
+type LinkProps = {
+  to: string;
+}
+
+const ListLink: React.FC<LinkProps> = ({children, to}) => (
+  <li style={{ display: `inline-block`, marginRight: `1rem` }}>
+    <Link to={to}>{children}</Link>
+  </li>
+)
 
 const Layout:React.FC = ({children}) => (
   <div style={{ margin: `3rem auto`, maxWidth: 650, padding: `0 1rem` }}>
-    <h3>MySweetSite</h3>
+    <header style={{ marginBottom: `1.5rem` }}>
+      <Link to="/" style={{ textShadow: `none`, backgroundImage: `none` }}>
+        <h3 style={{ display: `inline` }}>MySweetSite</h3>
+      </Link>
+      <ul style={{ listStyle: `none`, float: `right` }}>
+        <ListLink to="/">Home</ListLink>
+        <ListLink to="/about/">About</ListLink>
+        <ListLink to="/contact/">Contact</ListLink>
+      </ul>
+    </header>
     {children}
   </div>
 )
